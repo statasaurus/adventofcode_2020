f0256aacaa85193610c68e3bbe957f2edf8c8451
statasaurus <55274484+statasaurus@users.noreply.github.com>
2020-12-02 21:16:14 +0000

    Day 2 :snowman:

--- day_2.R
+++ day_2.R
@@ -10,5 +10,14 @@ passwords <- dat %>%
          high = str_extract(X1, "\\d{1,2}$") %>% as.integer(),
          n = str_count(X3, X2)) %>%
   filter(n >= low, n <= high)
-# Part 1
+dim(passwords)[1]
+
+# Part 2 
+passwords <- dat %>% 
+  mutate(X2 = str_remove(X2, ":"),
+         loc1 = str_extract(X1, "^\\d{1,2}") %>% as.integer(),
+         loc2 = str_extract(X1, "\\d{1,2}$") %>% as.integer(),
+         test1 = str_sub(X3, start = loc1, end = loc1) == X2,
+         test2 = str_sub(X3, start = loc2, end = loc2) == X2) %>% 
+  filter(test1 + test2 == 1)
 dim(passwords)[1]
