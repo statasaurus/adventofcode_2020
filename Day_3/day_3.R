ae3c66d094adf825df617a4e9a9a444ef510bc4b
statasaurus <55274484+statasaurus@users.noreply.github.com>
2020-12-03 19:46:08 +0000

    :snowflake: clean-up part 1 and finish part 2 :christmas_tree:

--- Day_3/day_3.R
+++ Day_3/day_3.R
@@ -2,37 +2,41 @@ library(readr)
 library(stringr)
 library(purrr)
 
-dat <- read_table("Day_3/day3_dat.txt", col_names = FALSE) 
-temp_length <- dat %>% 
-  pull(X1) %>% 
-  str_length() %>% 
-  unique()
-  
-map_matrix <- dat %>% 
-  mutate(split_dat = str_split(X1, "")) %>% 
-  pull(split_dat) %>% 
-  unlist() %>%
-  matrix(., ncol = temp_length, byrow = TRUE)
+dat <- read_lines("Day_3/day3_dat.txt") 
 
-path_end = dim(map_matrix)[1]
+map_matrix <- dat %>% 
+  str_split("") %>% 
+  do.call("rbind", .)
 
 # Part 1 
-right = 3 
-down = 1 
+n_trees <- function(map_matrix, right, down){
+  path_end <- dim(map_matrix)[1]
+  path_rows <- seq(1, path_end, by = down)
+  path_cols <- seq(1, by = right, length.out = length(path_rows))
+  
+  # The minimum width 
+  width = max(path_cols)
+  n_reps = ceiling(width/dim(map_matrix)[2])
+  
+  full_map <- do.call("cbind", rep(list(map_matrix), n_reps))
+  path <- map2_chr(path_rows, path_cols, function(x, y){
+    full_map[x,y]
+  })
+  
+  path %>% 
+    str_detect("#") %>% 
+    sum()
+}
 
-path_rows <- seq(1, path_end, by = down)
-path_cols <- c(1, path_rows*right+1)[-324]
+n_trees(map_matrix, right = 3, down = 1)
 
-# The minium width 
-width = max(path_cols)
-n_reps = ceiling(width/dim(map_matrix)[2])
+# Part 2 
+path1 <- n_trees(map_matrix, right = 1, down = 1)
+path2 <- n_trees(map_matrix, right = 3, down = 1)
+path3 <- n_trees(map_matrix, right = 5, down = 1)
+path4 <- n_trees(map_matrix, right = 7, down = 1)
+path5 <- n_trees(map_matrix, right = 1, down = 2)
+path1*path2*path3*path4*path5
 
-full_map <- rep(map_matrix, n_reps) %>% 
-  matrix(ncol = temp_length*n_reps)
-path <- map2_chr(path_rows, path_cols, function(x, y){
-  full_map[x,y]
-})
 
-path %>% 
-  str_detect("#") %>% 
-  sum()
+
