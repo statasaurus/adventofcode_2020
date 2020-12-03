d10a139e9c072164e80a3c5a1b1f84a34fc793f0
statasaurus <55274484+statasaurus@users.noreply.github.com>
2020-12-03 23:23:22 +0000

    :tada: Finish day 3

--- Day_3/day_3.py
+++ Day_3/day_3.py
@@ -3,25 +3,39 @@
 dat = open("/Users/christina/Documents/adventofcode_2020/Day_3/day3_dat.txt").readlines()
 dat = [x.replace("\n", "") for x in dat]
 
-# Get the slope 
-right = 3 
-down = 1
-slope = down / right
+def tree_count(dat, right, down):
+    # Get the slope 
+    slope = down / right
 
-# Get the x and y corrdinates 
-total_dist = len(dat)
-loc_y = [x for x in range(total_dist) if x % down == 0]
-loc_x = [int(x/slope) for x in loc_y]
+    # Get the x and y corrdinates 
+    total_dist = len(dat)
+    loc_y = [x for x in range(total_dist) if x % down == 0]
+    loc_x = [int(x/slope) for x in loc_y]
 
-# Count the trees 
-n_trees = 0 
-for i, ln in enumerate(dat): 
-    n_repeats = max(loc_x) // len(ln) + 1
-    rep_ln = ln * n_repeats
-    if(i in loc_y and rep_ln[loc_x[i]] == "#"):
-        n_trees = n_trees + 1
-         
-print(n_trees)
+    # Count the trees 
+    n_trees = 0 
+    for i, ln in enumerate(dat): 
+        n_repeats = max(loc_x) // len(ln) + 1
+        rep_ln = ln * n_repeats
+        step = int(i / down)
+        if(i in loc_y and rep_ln[loc_x[step]] == "#"):
+            n_trees = n_trees + 1
+            
+    return(n_trees)
+
+# Part 1
+print(tree_count(dat, right = 3, down = 1))
+
+# Part 2 
+track1 = tree_count(dat, right = 1, down = 1)
+track2 = tree_count(dat, right = 3, down = 1)
+track3 = tree_count(dat, right = 5, down = 1)
+track4 = tree_count(dat, right = 7, down = 1)
+track5 = tree_count(dat, right = 1, down = 2)
+print(track1 * track2 * track3 * track4 * track5)
+
+
+
         
 
 
